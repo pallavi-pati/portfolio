441c4687b158127bba7653f7361a3f8e3af61e74
pallavi-pati <89530884+pallavi-pati@users.noreply.github.com>
2024-12-21 01:56:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ document.querySelectorAll('header nav ul li a, .side-menu ul li a').forEach(link
     });
 });
 
-// Hamburger Menu Toggle
 const hamburger = document.querySelector('.hamburger-menu');
 const sideMenu = document.querySelector('.side-menu');
 
 // Toggle Side Menu
 hamburger.addEventListener('click', () => {
     if (sideMenu.style.right === '0px') {
-        sideMenu.style.right = '-250px';
+        sideMenu.style.right = '-250px'; // Hide the menu
     } else {
-        sideMenu.style.right = '0px';
+        sideMenu.style.right = '0px'; // Show the menu
     }
 });
