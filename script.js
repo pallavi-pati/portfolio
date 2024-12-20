fe54e567a6e44dbcc558fcfe3175abe4ea4c540d
pallavi-pati <89530884+pallavi-pati@users.noreply.github.com>
2024-12-21 01:54:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-// script.js
-document.querySelectorAll('header nav ul li a').forEach(link => {
+// Smooth Scrolling for Section Links
+document.querySelectorAll('header nav ul li a, .side-menu ul li a').forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
         const target = document.querySelector(e.target.getAttribute('href'));
         target.scrollIntoView({ behavior: 'smooth' });
     });
+});
+
+// Hamburger Menu Toggle
+const hamburger = document.querySelector('.hamburger-menu');
+const sideMenu = document.querySelector('.side-menu');
+
+// Toggle Side Menu
+hamburger.addEventListener('click', () => {
+    if (sideMenu.style.right === '0px') {
+        sideMenu.style.right = '-250px';
+    } else {
+        sideMenu.style.right = '0px';
+    }
 });
