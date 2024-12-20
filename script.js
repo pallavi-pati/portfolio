b9e9f7149e12e17d2f76dae93a338a41af46b276
pallavi-pati <89530884+pallavi-pati@users.noreply.github.com>
2024-12-21 01:46:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelectorAll('header nav ul li a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
